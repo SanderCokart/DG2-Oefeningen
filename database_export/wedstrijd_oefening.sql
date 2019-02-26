4062f9e497320d5d0a2fdc46d7009b37d3968753
Lars van Vliet <123321@student.drenthecollege.nl>
2019-02-26 13:15:33 +0100

    de juiste database export

--- database_export/wedstrijd_oefening.sql
+++ database_export/wedstrijd_oefening.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 26 feb 2019 om 10:24
+-- Gegenereerd op: 26 feb 2019 om 13:14
 -- Serverversie: 10.1.35-MariaDB
 -- PHP-versie: 7.2.9
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `wedstrijd_oefening`
 --
+CREATE DATABASE IF NOT EXISTS `wedstrijd_oefening` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `wedstrijd_oefening`;
 
 -- --------------------------------------------------------
 
@@ -41,7 +43,11 @@ CREATE TABLE `clubs` (
 --
 
 INSERT INTO `clubs` (`id`, `club_name`, `removed`, `reason`, `solution`) VALUES
-(9, 'Feyenoord', 0, NULL, NULL);
+(1, 'guacamole nigga penis', 1, 'r', 'r'),
+(2, 'nigga toilet', 1, 'r', 'r'),
+(3, 'd', 1, 'r', 'r'),
+(4, '5', 1, 'Jeff', 'Jeff'),
+(5, 'LOLD', 0, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -55,6 +61,13 @@ CREATE TABLE `games` (
   `score` varchar(3) NOT NULL,
   `club_2_name` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `games`
+--
+
+INSERT INTO `games` (`id`, `club_1_name`, `score`, `club_2_name`) VALUES
+(1, 'Feyenoord', '2-1', 'Ajax');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -81,13 +94,13 @@ ALTER TABLE `games`
 -- AUTO_INCREMENT voor een tabel `clubs`
 --
 ALTER TABLE `clubs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT voor een tabel `games`
 --
 ALTER TABLE `games`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
