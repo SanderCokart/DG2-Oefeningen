029e37a0a013e7894d23b8671b45b072f1a89d3c
Lars van Vliet <123321@student.drenthecollege.nl>
2019-02-26 16:04:24 +0100

    fout melding toegevoegd genaamd wrongId

---
+++ includes/tools.inc.php
@@ -0,0 +1,38 @@
+<?php
+// function for checking if the id exists
+function checkExistanceId($idToCheck, $table) {
+  include 'db.inc.php';
+  // switch ($table) {
+  //   case 'clubs':
+  //     $sql = "SELECT id FROM clubs WHERE removed=0";
+  //     break;
+  //
+  //   case 'games':
+  //     $sql = "SELECT id FROM games";
+  //     break;
+  //
+  //   default:
+  //     return false;
+  //     break;
+  if ($table == "clubs") {
+    $sql = "SELECT id FROM clubs WHERE removed=0";
+  } else if ($table == "games") {
+    $sql = "SELECT id FROM games";
+  }
+  $_SESSION['existingId'] = FALSE; // standard value is false
+  $result = $conn->query($sql);
+  while ($row = $result->fetch_assoc()) { // check if id exists in
+    if ($_POST[$idToCheck] == $row['id']) { // the id exists in table
+      $_SESSION['existingId'] = TRUE;
+      return true;
+    }
+  }
+}
+
+
+function wrongId() {
+  die ("The id doesn't exist or the id field has been left empty <br> <img src=https://accountingpl.us/assets/images/screenshots/treeguy.jpg width='500'>");
+}
+
+
+ ?>
