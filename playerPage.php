dd0fdc85c85393c3ebc868906e74df93c6a220a8
Sander Cokart <112039@student.drenthecollege.nl>
2019-03-01 10:31:28 +0100

    made player page and restyled the tables

--- playerPage.php
+++ playerPage.php
@@ -2,9 +2,37 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="styles/player.css">
     <title></title>
   </head>
   <body>
 
+    <?php include "includes/db.inc.php"?>
+    <div id="T1">
+        <table width="100%">
+            <?php
+                echo "<tr>
+                <th>Club 1</th>
+                <th>Score</th>
+                <th>Club 2</th>
+                </tr>";
+
+                $result = $conn->query("SELECT * FROM games ORDER BY id;");
+
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>";
+                    echo $row['club_1_name'];
+                    echo "</td>";
+                    echo "<td>";
+                    echo $row['score'];
+                    echo "</td>";
+                    echo "<td>";
+                    echo $row['club_2_name'];
+                    echo "</td>";
+                }
+             ?>
+        </table>
+    </div>
   </body>
 </html>
